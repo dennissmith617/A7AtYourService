fa2a3e6aba88693906223e66420488991809db1d
Dennis Smith <dennis.smith@richmond.edu>
2022-11-02 21:01:24 -0400

    Connecting to DB

--- app/src/main/java/com/example/a7atyourservice/StickItActivity.java
+++ app/src/main/java/com/example/a7atyourservice/StickItActivity.java
@@ -1,13 +1,72 @@
 package com.example.a7atyourservice;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class StickItActivity extends Activity {
 
+    private static final String TAG = StickItActivity.class.getSimpleName();
+
+    private DatabaseReference mDatabase;
+
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stickit);
+
+        // Connect with firebase
+        //
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+
+
+    // Add data to firebase button
+    public void doAddDataToDb(View view) {
+        // Write a message to the database
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("message");
+
+        Task t = myRef.setValue("Hello, World!");
+        Log.i(TAG, "doAddDataToDb: " + t.getResult());
+        if(!t.isSuccessful()){
+            Toast.makeText(getApplicationContext()
+                    , "Failed to write value into firebase. " , Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Read from the database by listening for a change to that item.
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+                TextView tv = (TextView) findViewById(R.id.username);
+                tv.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+                Toast.makeText(getApplicationContext()
+                        , "Failed to write value into firebase. " , Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
     }
 }
