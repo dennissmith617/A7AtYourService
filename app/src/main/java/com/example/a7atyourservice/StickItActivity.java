4a86f941f4697a14b2f3f27432602ee3be905858
Dennis Smith <dennis.smith@richmond.edu>
2022-11-03 21:45:03 -0400

    Select image and update DB

--- app/src/main/java/com/example/a7atyourservice/StickItActivity.java
+++ app/src/main/java/com/example/a7atyourservice/StickItActivity.java
@@ -1,20 +1,27 @@
 package com.example.a7atyourservice;
 
 import android.annotation.SuppressLint;
-import android.app.ListActivity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -22,6 +29,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.example.a7atyourservice.User;
+import com.google.firebase.database.ServerValue;
 
 import java.util.ArrayList;
 
@@ -32,9 +40,13 @@ public class StickItActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private ImageButton smiley;
     private Button loginButton;
+    private Button sendButton;
     private EditText loginText;
     private TextView usernameView;
     private TextView friendsList;
+    private EditText recipientText;
+
+    private String selectedSticker;
 
 
     @SuppressLint("RestrictedApi")
@@ -46,11 +58,16 @@ public class StickItActivity extends AppCompatActivity {
         // Connect with firebase
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        // Create notification channel
+        createNotificationChannel();
+
         loginText = (EditText) findViewById(R.id.username);
         loginButton = findViewById(R.id.login);
-        smiley = findViewById(R.id.firebaseImage);
+        sendButton = findViewById(R.id.send_button);
+        smiley = findViewById(R.id.smiley_sticker);
         usernameView = findViewById(R.id.username_view);
         friendsList = findViewById(R.id.friends_list);
+        recipientText = findViewById(R.id.recipient);
 
 
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -64,7 +81,15 @@ public class StickItActivity extends AppCompatActivity {
         smiley.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addStickerToDB(usernameView.getText().toString(), smiley.getTransitionName());
+                selectSticker(smiley);
+            }
+        });
+
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendSticker(usernameView.getText().toString(), recipientText.getText().toString());
             }
         });
 
@@ -80,6 +105,7 @@ public class StickItActivity extends AppCompatActivity {
 
                     @Override
                     public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                        sendNotification();
                         Log.v(TAG, "onChildChanged: " + dataSnapshot.getValue().toString());
                     }
 
@@ -103,38 +129,87 @@ public class StickItActivity extends AppCompatActivity {
         );
     }
 
-    public void addStickerToDB(String username, String image_id) {
-        // Write a message to the database
-        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference(String.valueOf(username));
+    public void selectSticker(ImageButton sticker) {
+        selectedSticker = sticker.getTransitionName();
+        // selected sticker is tinted red
+        sticker.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+    }
 
-        Task t = myRef.setValue(image_id);
-        if (!t.isSuccessful()) {
-            Toast.makeText(getApplicationContext()
-                    , "Failed to write value into firebase. ", Toast.LENGTH_SHORT).show();
-        }
+    public void sendSticker(String curr_username, String friend_username) {
+        DatabaseReference curr_user = mDatabase.child("users").child(curr_username);
+        DatabaseReference to_user = mDatabase.child("users").child(friend_username);
+
+        // increment
+        DatabaseReference curr_user_sent_count= curr_user.child("stickersSent");
+        DatabaseReference to_user_sticker_count = to_user.child("stickersRecieved");
+
+
+        curr_user_sent_count.setValue(ServerValue.increment(1));
+        to_user_sticker_count.setValue(ServerValue.increment(1));
     }
 
     // Add Users to DB
     public void addUser(String username) {
 
         User user;
-        user = new User(username, "0");
+        // Start off with no stickers
+        user = new User(username, 0, 0);
         Task t1 = mDatabase.child("users").child(user.username).setValue(user);
 
         if(!t1.isSuccessful()){
             Toast.makeText(getApplicationContext(),"Unable to login!",Toast.LENGTH_SHORT).show();
         }
         String displayText = "username: " + loginText.getText().toString();
-        usernameView.setText(displayText);
-        smiley.setVisibility(View.VISIBLE);
+
+        // Hide Login Info
         loginButton.setVisibility(View.INVISIBLE);
         loginText.setVisibility(View.INVISIBLE);
+
+        // Show sticker "view"
+        usernameView.setText(displayText);
+        smiley.setVisibility(View.VISIBLE);
+        recipientText.setVisibility(View.VISIBLE);
+        sendButton.setVisibility(View.VISIBLE);
     }
 
     public void displayFriends(DataSnapshot dataSnapshot) {
         User user = dataSnapshot.getValue(User.class);
         String current_friends = friendsList.getText().toString();
-        String new_friends_list = current_friends + ", " + user.username;
+        String new_friends_list = "Friends: " + current_friends + user.username + ", ";
         friendsList.setText(new_friends_list);
+    }
+
+    // code here from -> https://developer.android.com/develop/ui/views/notifications/build-notification
+    private void createNotificationChannel() {
+        // Create the NotificationChannel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(getString(R.string.channel_id), name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void sendNotification() {
+
+
+        // Sticker notification
+        String channelId = getString(R.string.channel_id);
+        NotificationCompat.Builder notifyBuild = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.party)
+                .setContentTitle("New sticker alert!")
+                .setContentText("Subject")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                // hide the notification after its selected
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        // // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(0, notifyBuild.build());
+
     }
 }
