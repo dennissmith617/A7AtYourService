e6ad006cbe2f94b39fdb415ed7a578d948b9081e
Dennis Smith <dennis.smith@richmond.edu>
2022-11-14 14:58:40 -0500

    Added button to open app, creating bottom nav bar

--- app/src/main/java/com/example/a7atyourservice/SmartFitSocial.java
+++ app/src/main/java/com/example/a7atyourservice/SmartFitSocial.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SmartFitMainActivity extends AppCompatActivity {
+public class SmartFitSocial extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sf_main);
+        setContentView(R.layout.activity_sf_social);
     }
 }
