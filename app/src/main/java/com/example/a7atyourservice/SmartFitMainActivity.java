2804cf3241107dd70b8b5e422b1e8a1e6e34aafa
Dennis Smith <dennis.smith@richmond.edu>
2022-11-15 19:43:37 -0500

    Refactoring Menu Bar to include fragments

--- app/src/main/java/com/example/a7atyourservice/SmartFitMainActivity.java
+++ app/src/main/java/com/example/a7atyourservice/SmartFitMainActivity.java
@@ -3,11 +3,50 @@ package com.example.a7atyourservice;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.a7atyourservice.databinding.ActivitySfMainBinding;
 
 public class SmartFitMainActivity extends AppCompatActivity {
+
+    ActivitySfMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sf_main);
+        binding = ActivitySfMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.camera:
+                    replaceFragment(new CameraFragment());
+                    break;
+                case R.id.diet:
+                    replaceFragment(new DietFragment());
+                    break;
+                case R.id.social:
+                    replaceFragment(new SocialFragment());
+                    break;
+                case R.id.lifting:
+                    replaceFragment(new LiftingFragment());
+                    break;
+
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
