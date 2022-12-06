f120aa9a9e67a55ed5448fd6efc0da462e19a52e
yau.m <yau.m@northeastern.edu>
2022-12-05 23:37:17 -0500

    connected the home screen calendar to the database, now able to check in and read workout data from the db

--- app/src/main/java/com/example/a7atyourservice/Helpers.java
+++ app/src/main/java/com/example/a7atyourservice/Helpers.java
@@ -6,7 +6,11 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Helpers {
 
@@ -19,4 +23,11 @@ public class Helpers {
         return FirebaseFirestore.getInstance().collection("workouts").
                 document(firebaseUser.getUid()).collection("my_workouts");
     }
+    public static DocumentReference getCheckRef() {
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        return FirebaseFirestore.getInstance().collection("Check").
+                document(firebaseUser.getUid());
+    }
+
 }
--- app/src/main/java/com/example/a7atyourservice/SmartFitMainActivity.java
+++ app/src/main/java/com/example/a7atyourservice/SmartFitMainActivity.java
@@ -1,29 +1,43 @@
 package com.example.a7atyourservice;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import com.applandeo.materialcalendarview.CalendarView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
+import com.example.a7atyourservice.model.Check;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SmartFitMainActivity extends AppCompatActivity {
 
     private CalendarView calendarView;
     private Button checkinButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +45,96 @@ public class SmartFitMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sf_main);
         // set up calendar
         calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                Intent intent = new Intent(SmartFitMainActivity.this, DayActivity.class);
+                intent.putExtra("day",eventDay.getCalendar().getTimeInMillis());
+                startActivity(intent);
+            }
+        });
+
+        // set up check in button
         checkinButton = findViewById(R.id.checkinButton);
+        checkinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseFirestore.getInstance().collection("Check")
+                        . document(FirebaseAuth.getInstance().getUid())
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()){
+                                    DocumentSnapshot snapshot = task.getResult();
+                                    List<String> list = null;
+                                    try {
+                                        if (snapshot != null && snapshot.exists()) {
+                                            Check check = snapshot.toObject(Check.class);
+                                            list = check.time;
+                                        }
+                                    } catch (Exception ex) {
+                                        ex.printStackTrace();
+                                    }
+                                    if (list==null){
+                                        list = new ArrayList<>();
+                                    }
+
+                                    Date totay = new Date();
+                                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                                    String todayStr = sdf.format(totay);
+                                    if (list.contains(todayStr)){
+                                        return;
+                                    }
+                                    list.add(todayStr);
+                                    Map<String, Object> data =  new HashMap<>();
+                                    data.put("uid",FirebaseAuth.getInstance().getUid());
+                                    data.put("time",list);
+                                    Helpers.getCheckRef().set(data)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void unused) {
+
+                                                }
+                                            });
+                                }
+                            }
+                        });
+
+                ;
+
+            }
+        });
 
         // add checked in dates
-        List<EventDay> events = new ArrayList<>();
-        Calendar calendar1 = Calendar.getInstance();
-        calendar1.set(2022,11,1);
-        Calendar calendar2 = Calendar.getInstance();
-        calendar2.set(2022,11,4);
-        events.add(new EventDay(calendar1, R.drawable.check));
-        events.add(new EventDay(calendar2, R.drawable.check));
-        calendarView.setEvents(events);
+        FirebaseFirestore.getInstance().collection("Check")
+                . document(FirebaseAuth.getInstance().getUid())
+                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (value.exists()) {
+                            List<String> list = null;
+                            try {
+                                Check check = value.toObject(Check.class);
+                                list = check.time;
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                            if (list != null) {
+                                List<EventDay> events = new ArrayList<>();
+                                for (int i = 0; i < list.size(); i++) {
+                                    String temp = list.get(i);
+                                    Date date = stringToDate(temp);
+                                    Calendar calendar = Calendar.getInstance();
+                                    calendar.setTime(date);
+                                    events.add(new EventDay(calendar, R.drawable.check));
+                                }
+                                calendarView.setEvents(events);
+                            }
+                        }
+                    }
+                });
+
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
@@ -76,5 +169,17 @@ public class SmartFitMainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    public  Date stringToDate(String strTime)
+    {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try {
+            date = formatter.parse(strTime);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
     }
 }
