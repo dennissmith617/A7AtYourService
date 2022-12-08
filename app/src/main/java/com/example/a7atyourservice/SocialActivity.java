e4dc6d94dd7697b5ff82b407fae88a907da31e9e
Dennis Smith <dennis.smith@richmond.edu>
2022-12-07 22:00:17 -0500

    Cleanup, saving and sharing photos

--- app/src/main/java/com/example/a7atyourservice/SocialActivity.java
+++ app/src/main/java/com/example/a7atyourservice/SocialActivity.java
@@ -1,61 +1,21 @@
 package com.example.a7atyourservice;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.a7atyourservice.model.ChatMessaging;
-import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
 
 public class SocialActivity extends AppCompatActivity {
-    private FirebaseAnalytics mFirebaseAnalytics;
-    //private FirebaseListAdapter<ChatMessaging> adapter;
-
-    ListView lvChat;
-    ArrayList<String> listOfDiscussion = new ArrayList<String>();
-    ArrayAdapter arrayAdpt;
-
-    String UserName;
-
-    private DatabaseReference dbr = FirebaseDatabase.getInstance().getReference().getRoot();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social);
-
-        // initialize firebase
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
@@ -91,106 +51,6 @@ public class SocialActivity extends AppCompatActivity {
             }
         });
 
-
-        // Delete commented out part later
-        /*
-        FloatingActionButton chatbutton =
-                (FloatingActionButton)findViewById(R.id.chatbutton);
-
-        chatbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText input = (EditText)findViewById(R.id.input);
-
-                // Read the input field and push a new instance
-                // of ChatMessage to the Firebase database
-                FirebaseDatabase.getInstance()
-                        .getReference()
-                        .push()
-                        .setValue(new ChatMessaging(input.getText().toString(),
-                                FirebaseAuth.getInstance()
-                                        .getCurrentUser()
-                                        .getDisplayName())
-                        );
-
-                // Clear the input
-                input.setText("");
-            }
-        });
-        displayChatMessages();
-
-         */
-
-
-        lvChat = (ListView) findViewById(R.id.lvChat);
-        arrayAdpt = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, listOfDiscussion);
-        lvChat.setAdapter(arrayAdpt);
-
-        getUserName();
-
-        dbr.addValueEventListener(new ValueEventListener(){
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Set<String> set = new HashSet<String>();
-                Iterator i = dataSnapshot.getChildren().iterator();
-
-                while(i.hasNext()){
-                    set.add(((DataSnapshot)i.next()).getKey());
-                }
-
-                arrayAdpt.clear();
-                arrayAdpt.addAll(set);
-                arrayAdpt.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-
-
-
-
-        });
-
-        lvChat.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(getApplicationContext(), DiscussionActivity.class);
-                i.putExtra("selected_topic", ((TextView)view).getText().toString());
-                i.putExtra("user_name", UserName);
-                startActivity(i);
-            }
-        });
-
     }
-    private void getUserName(){
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        final EditText userName = new EditText(this);
-
-        builder.setView(userName);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                UserName = userName.getText().toString();
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                getUserName();
-            }
-        });
-        builder.show();
-    }
-
-    //IGNORE BELOW, delete later
-    /*private void displayChatMessages(){
-
-    }
-
-     */
-
 
 }
