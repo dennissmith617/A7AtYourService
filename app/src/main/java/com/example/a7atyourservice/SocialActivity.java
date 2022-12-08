3a5867a0504bf2cb93b8b2666cacbbea53f3b393
clselva <selva.c@northeastern.edu>
2022-12-07 21:43:45 -0500

    1

--- app/src/main/java/com/example/a7atyourservice/SocialActivity.java
+++ app/src/main/java/com/example/a7atyourservice/SocialActivity.java
@@ -9,17 +9,31 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.a7atyourservice.model.ChatMessaging;
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SocialActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseListAdapter<ChatMessaging> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social);
+
+        // initialize firebase
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
@@ -55,6 +69,40 @@ public class SocialActivity extends AppCompatActivity {
             }
         });
 
+        FloatingActionButton chatbutton =
+                (FloatingActionButton)findViewById(R.id.chatbutton);
+
+        chatbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)findViewById(R.id.input);
+
+                // Read the input field and push a new instance
+                // of ChatMessage to the Firebase database
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessaging(input.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+
+                // Clear the input
+                input.setText("");
+            }
+        });
+        displayChatMessages();
+
+
+
+
+
     }
+
+    private void displayChatMessages(){
+
+    }
+
 
 }
