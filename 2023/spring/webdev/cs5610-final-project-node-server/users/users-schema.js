57b592a9819456b2bb3ab75d3942a44f4c0d03a0
Dennis Smith <dennis.smith@richmond.edu>
2023-04-01 21:36:08 -0400

    Updating code

---
+++ 2023/spring/webdev/cs5610-final-project-node-server/users/users-schema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+
+const usersSchema = new mongoose.Schema(
+    {
+        username: { type: String, unique: true, required: true },
+        password: { type: String, required: true },
+        firstName: String,
+        lastName: String,
+        email: { type: String, unique: true },
+        age: Number,
+        booksRead: { type: Number, default: 0 },
+        role: { type: String, default: "VIEWER", enum: ["VIEWER", "AUTHOR", "CRITIC", "ADMIN"] },
+    },
+    {
+        collection: "users",
+    }
+);
+export default usersSchema;
